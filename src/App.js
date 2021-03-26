cd0063da38e9ae5f4f3a180587361c75fcd4d088
adamcanray <adamcool180@gmail.com>
2021-03-27 01:44:42 +0700

    conf(app): wrap App with Suspense and Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { hot } from 'react-hot-loader/root';
+import { Switch, Route, Redirect } from "react-router-dom";
+import Layout from './containers/Layout';
+
 class App extends React.Component {    
    render() {   
       const { name } = this.props; 
-      return (<><h1> Hello {name}</h1></>);
+      return (
+         <>
+         <Switch>
+            <Redirect exact from="/" to="/app" />
+            <Route path="/app" component={Layout} />
+         </Switch>
+         </>
+      );
    }
 }
 export default hot(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router} from 'react-router-dom';
 import App from "./App";
 var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Adam Canray" />, mountNode);
+ReactDOM.render(
+  <Suspense fallback={
+    <div>Loading...</div>
+  }>
+    <Router>
+      <App />
+    </Router>
+  </Suspense>
+, mountNode);
