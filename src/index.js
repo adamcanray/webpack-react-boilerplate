056ce57054b095d58571e2783e34a6a8a2cf4a35
adamcanray <adamcool180@gmail.com>
2021-03-27 00:01:23 +0700

    feat(app): create main files react

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Adam Canray" />, mountNode);
